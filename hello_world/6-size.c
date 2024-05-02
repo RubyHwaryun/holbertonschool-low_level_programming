29790d5bd497a13192426b3aa60887fa78ee275b
RubyHwaryun <abiyev.rasul44@gmail.com>
2024-05-02 13:21:04 +0000

    changes to pass betty

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Prints the size of various types based on
+ *        the computer it is compiled and run on..
+ *
+ * Return: Always 0.
+ */
+
 int main(void)
 {
 	int a;

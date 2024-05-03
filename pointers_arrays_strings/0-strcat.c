fe7f6509a66d24b52dbbc8aa9e0783c743a2c399
RubyHwaryun <abiyev.rasul44@gmail.com>
2024-05-03 18:16:19 +0400

    error fixing v1

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  * strcat - Concatenates the string pointed to by @src, including the terminating
  *          null byte, to the end of the string pointed to by @dest.
@@ -11,13 +10,16 @@
  */
 char *strcat(char *dest, const char *src)
 {
-	int index = 0, dest_len = 0;
+        int index = 0, dest_len = 0;
 
-	while (dest[index++])
-		dest_len++;
+        while (dest[index++] != '\0')
+                dest_len++;
+        index = 0;
+  
+        for (index = 0; src[index]; index++)
+                dest[dest_len++] = src[index];
+        
+        dest[dest_len] = '\0';
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
-	return (dest);
+        return (dest);
 }
